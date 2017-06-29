be5e2621550e97c9fad3f8828175c218ac027683
Mark <lixiaoye37@users.noreply.github.com>
2017-06-29 04:23:26 -0700

    HOMEWORK2

--- app/src/main/java/com/example/mark/newsapp/NetworkUtils.java
+++ app/src/main/java/com/example/mark/newsapp/NetworkUtils.java
@@ -2,11 +2,16 @@ package com.example.mark.newsapp;
 
 import android.net.Uri;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -57,5 +62,23 @@ public class NetworkUtils {
         } finally {
             urlConnection.disconnect();
         }
+    }
+    public static ArrayList<NewsItem> parseJSON(String json) throws JSONException {
+        ArrayList<NewsItem> result = new ArrayList<>();
+        JSONObject main = new JSONObject(json);
+        JSONArray items = main.getJSONArray("items");
+
+        for(int i = 0; i < items.length(); i++){
+            JSONObject item = items.getJSONObject(i);
+            String author = item.getString("author");
+            String title = item.getString("title");
+            String description = item.getString("description");
+            String url = item.getString("url");
+            String urlToImage = item.getString("urlToImage");
+            String publishedAt = item.getString("publishedAt");
+            NewsItem repo = new NewsItem(author, title,description, url,urlToImage, publishedAt);
+            result.add(repo);
+        }
+        return result;
     }
 }
